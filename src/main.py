1f927f0f25245a3bc73b3fde19f1ad68a758f570
Robin W <123robinabc@gmail.com>
2021-07-30 22:55:44 -0700

    Prints ready message and user messages to console

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 import discord
 from discord.ext import commands
+
 import os
 
 bot = commands.Bot(command_prefix='>')
+
+@bot.event
+async def on_ready():
+  print('{0.user} is online'.format(bot))
+
+@bot.event
+async def on_message(message):
+  # Ignore bot messages including self
+  user_message = message.author
+  if user_message == bot.user or user_message.bot == True:
+    return
+  print(user_message.name + '#' + user_message.discriminator + ": " + message.content)
+
 
 if __name__ == "__main__":
   # Same as obtaining 'cd ../.bot_token' path
